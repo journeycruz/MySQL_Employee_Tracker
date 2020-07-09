bc7601b869ee51d6bc0e5a584596da3693fe1ded
Journey Cruz <journey.cruz@gmail.com>
2020-07-09 16:09:12 -0500

    add function for view employees

--- server.js
+++ server.js
@@ -2,37 +2,34 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "top_songsDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "top_songsDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  promptUser();
+connection.connect(function (err) {
+    if (err) throw err;
+    promptUser();
 });
 
 function promptUser() {
-    inquirer.prompt([
-        {
-            name: "userChoice",
-            message: "What do you want to do?",
-            type: "list",
-            choices: 
-            [
-                "Add a department",
-                "Add an employee",
-                "Add a role",
-                "View departments",
-                "View employees",
-                "View roles",
-                "Update Employee Roles",
-                "No need to do any more."
-            ]
-        }
-    ]).then(function(data) {
+    inquirer.prompt([{
+        name: "userChoice",
+        message: "What do you want to do?",
+        type: "list",
+        choices: [
+            "Add a department",
+            "Add an employee",
+            "Add a role",
+            "View departments",
+            "View employees",
+            "View roles",
+            "Update Employee Roles",
+            "No need to do any more."
+        ]
+    }]).then(function (data) {
         switch (data.userChoice) {
             case "Add a department":
                 addDepartment();
@@ -69,3 +66,39 @@ function promptUser() {
     })
 }
 
+function addDepartment() {
+
+}
+
+function addEmployee() {
+
+}
+
+function addRole() {
+
+}
+
+function viewDepartments() {
+
+}
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.log("\n Employees currently in personnel_db \n");
+        console.table(res);
+    })
+    promptUser();
+}
+
+function viewRoles() {
+
+}
+
+function updateEmployeeRoles() {
+
+}
+
+function stop() {
+
+}
